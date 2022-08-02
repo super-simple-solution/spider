63ba6088e3a66d291f6511ce284703ff43f00965
wuxue <wudaxue123@gmail.com>
2022-08-02 21:54:30 +0800

    测试

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const playwright = require('playwright');
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+(async () => {
+  const browser = await playwright.chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://native-land.ca/');
+  await page.screenshot({ path: 'example1.png' });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // create a locator
-  const getStarted = page.locator('text=Get Started');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
- 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  await browser.close();
+})();
