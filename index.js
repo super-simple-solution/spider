754bd3280741b0c17e4b5e4b49ee1fc2560a5dc9
wuxue <wudaxue123@gmail.com>
2022-08-03 18:44:23 +0800

    抓取列表

---
+++ index.js
@@ -0,0 +1,28 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext({
+    javaScriptEnabled: false
+  });
+  const page = await context.newPage();
+  await page.goto('https://book.douban.com/');
+  await page.waitForTimeout(1000)
+
+  const list = await page.$$eval('.slide-item li', doms => {
+    return doms.map(dom => {
+      const cover = dom.querySelector('.cover img')
+      const title = dom.querySelector('.title')
+      const author = dom.querySelector('.author')
+      const href = dom.querySelector('.cover a')
+      return {
+        cover: cover && cover.getAttribute('src'),
+        title: title && title.innerText,
+        author: author && author.innerText,
+        href: href && href.getAttribute('href')
+      }
+    })
+  })
+  console.log(list, 444444)
+  await browser.close();
+})()
